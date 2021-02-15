27b1a08029a6cccdbed7dc403c7d49e777296d0a
yusuf <vadapallimumtaj123@gmail.com>
2021-02-15 21:11:16 +0530

    c96

--- Screens/AlarmScreen.js
+++ Screens/AlarmScreen.js
@@ -1,8 +1,8 @@
 import * as React from "react";
 import {Text,View,TouchableOpacity,TextInput,StyleSheet} from "react-native";
 import firebase from "firebase";
-import{ Header} from "react-native-elements"
-
+import{ Header} from "react-native-elements";
+import * as Notifications from "expo-notifications";
 
 
 export default class AlarmScreen extends React.Component{
@@ -12,6 +12,20 @@ export default class AlarmScreen extends React.Component{
             hour:null
         }
     }
+
+    setAlarm=()=>{
+        Notifications.scheduleNotificationAsync({
+            content:{
+                title: "Water Alarm",body:"You Need To Drink Water"
+            },
+            trigger:{
+                
+                hours:this.state.hour,
+                repeats:true
+            }
+        })
+    }
+
     render(){
         return(
             
@@ -24,6 +38,9 @@ export default class AlarmScreen extends React.Component{
                 />
 
            
+           <Text>
+               Add After How frequently you want the notifications you want the notification
+           </Text>
 
             <TextInput
             placeholder="Set Alarm"
@@ -33,7 +50,26 @@ export default class AlarmScreen extends React.Component{
             }}
             />
 
-        
+            <TouchableOpacity  style={styles.button} 
+            onPress={()=>{
+                this.setAlarm()
+            }}
+            >
+                <Text style={styles.buttonText}>
+                    Start Alarm
+                </Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity  style={styles.button} 
+            onPress={()=>{
+                this.stopAlarm()
+            }}
+            >
+                <Text style={styles.buttonText}>
+                    Stop Receivig Alarm
+                </Text>
+            </TouchableOpacity>
+                
             </View>
         )
     }
@@ -51,4 +87,22 @@ const styles=StyleSheet.create({
         borderWidth:2,
         marginTop:10
     },
+    button:{
+        backgroundColor:"#FBE7C6",
+        padding:20,
+        alignItems:"center",
+        justifyContent:"center",
+        borderRadius:10,
+        borderWidth:2,
+        marginTop:10,
+        width:"50%",
+        alignSelf:"center"
+
+    },
+  
+    buttonText:{
+        fontSize:20,
+        fontWeight:"bold"
+    }
+    
 })
